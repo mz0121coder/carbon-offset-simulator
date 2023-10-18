5459b48e166a507c41bf625451965c795cb1ec2a
Mohammad Zafar <mzcoder7@gmail.com>
2023-10-18 18:02:54 +0100

    initial project setup

---
+++ simulator/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div className='text-center'>
+			<h1 className='mt-[2vw]'>Carbon Offset Simulator</h1>
+		</div>
+	);
+};
+
+export default App;
