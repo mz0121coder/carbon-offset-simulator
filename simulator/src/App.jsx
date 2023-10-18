891a1096bf94a4f80e5aff5624b8d443253cc0d1
Mohammad Zafar <mzcoder7@gmail.com>
2023-10-18 22:50:40 +0100

    use recoil for state management

--- simulator/src/App.jsx
+++ simulator/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './assets/mz-tech-logo.png';
+import { useRecoilState } from 'recoil';
+import { loggedInState } from './utils/atoms.js';
 
 const App = () => {
-	return (
+	const [loggedIn, setLoggedIn] = useRecoilState(loggedInState);
+
+	return !loggedIn ? (
 		<div className='flex flex-col items-center leading-loose gap-4'>
 			<img src={logo} alt='MZ tech logo' className='w-64 h-64' />
 			<h1 className='text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl'>
@@ -14,6 +18,12 @@ const App = () => {
 			<button className='mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>
 				Get started
 			</button>
+		</div>
+	) : (
+		<div className='flex flex-col items-center leading-loose gap-4'>
+			<h1 className='mt-[1.5vw] text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl'>
+				Carbon Offset Simulator
+			</h1>
 		</div>
 	);
 };
