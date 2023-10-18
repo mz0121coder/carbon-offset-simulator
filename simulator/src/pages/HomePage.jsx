8f509e4e54e87134e707a5207b8f47770cb4c01d
Mohammad Zafar <mzcoder7@gmail.com>
2023-10-18 23:49:54 +0100

    refactor App component

---
+++ simulator/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const HomePage = () => {
+	return (
+		<div className='flex flex-col items-center leading-loose gap-4'>
+			<div className='flex justify-between w-full items-center'>
+				<h1 className='mt-[1.5vw] text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl mx-auto'>
+					Carbon Offset Simulator
+				</h1>
+			</div>
+		</div>
+	);
+};
+export default HomePage;
