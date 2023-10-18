59e54d726c2f72733d3808c28dde5f99fda9b36b
Mohammad Zafar <mzcoder7@gmail.com>
2023-10-18 18:59:25 +0100

    start building homepage

--- simulator/src/App.jsx
+++ simulator/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import logo from './assets/mz-tech-logo.png';
 
 const App = () => {
 	return (
-		<div className='text-center'>
-			<h1 className='mt-[2vw]'>Carbon Offset Simulator</h1>
+		<div className='flex flex-col items-center leading-loose gap-4'>
+			<img src={logo} alt='MZ tech logo' className='w-64 h-64' />
+			<h1 className='text-lg font-bold md:text-xl lg:text-2xl xl:text-3xl'>
+				Carbon Offset Simulator
+			</h1>
+			<p className='text-center text-base md:text-lg lg:text-xl xl:text-2xl'>
+				Login or sign up to get started
+			</p>
+			<button className='mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>
+				Get started
+			</button>
 		</div>
 	);
 };
