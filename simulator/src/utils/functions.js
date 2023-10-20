2f1fbf01d95a1ae6d9b5f50ba1546e55947671e7
Mohammad Zafar <mzcoder7@gmail.com>
2023-10-20 15:48:20 +0100

    getSummary function(2)

--- simulator/src/utils/functions.js
+++ simulator/src/utils/functions.js
@@ -8,22 +8,8 @@ const sampleInputs = {
 	timeToGrow: 5,
 	inflation: 3.7,
 };
-/*
-take in inputs
-get current month in format 'October 2023' 
-monthly emissions = (annual consumption / 12) * 100
 
-
-use an array of objects to track trees planted and their offset
-
-*/
 export const getSummary = inputs => {
-	// current month in format 'October 2023'
-	const currentDate = new Date();
-	const currentMonthYear = currentDate.toLocaleString('en-US', {
-		month: 'long',
-		year: 'numeric',
-	});
 	// monthly emissions to offset
 	const monthlyEmissions = (inputs.annualConsumption / 12) * 1000;
 	// time for a tree to fully grow
@@ -36,27 +22,67 @@ export const getSummary = inputs => {
 	const monthlyInflation = inputs.inflation / 100 / 12;
 	const monthlyCost =
 		inputs.annualCost / 12 + monthlyInflation * (inputs.annualCost / 12);
-
+	let treeCount = 0;
 	const treesPlanted = [];
+	const summaryTable = [];
+
 	// update the values each month
 	while (offsetPerMonth < monthlyEmissions) {
+		treeCount += inputs.treesPerMonth;
 		months++;
 		cost += monthlyCost;
-		const newTree = { monthPlanted: months };
-		treesPlanted.push(newTree);
+		// this will be multiplied by trees per month
+		const newTrees = { monthPlanted: months, amount: inputs.treesPerMonth };
+		treesPlanted.push(newTrees);
 		// update the offset, using starting month for each tree
-		treesPlanted.slice(0, -1).forEach(tree => {
+		treesPlanted.forEach(tree => {
 			const monthsGrown = months - tree.monthPlanted;
 			// add full offset for a fully grown tree
 			if (monthsGrown >= monthsToGrow) {
-				offsetPerMonth += inputs.carbonOffset;
+				offsetPerMonth += inputs.treesPerMonth * (inputs.carbonOffset / 12);
 			} else {
 				// increase the amount offset each month until a tree is fully grown
-				const growthPerMonth = inputs.carbonOffset / monthsToGrow;
-				offsetPerMonth += growthPerMonth * monthsGrown;
+				const growthPerMonth = inputs.carbonOffset / 12 / monthsToGrow;
+				offsetPerMonth += inputs.treesPerMonth * (growthPerMonth * monthsGrown);
 			}
 		});
-		console.log({ months, offsetPerMonth, monthlyEmissions });
+
+		// current month in format 'October 2023'
+		const currentDate = new Date();
+		const currentMonthYear = currentDate.toLocaleString('en-US', {
+			month: 'long',
+			year: 'numeric',
+		});
+		// future month in same format
+		const futureDate = new Date(currentDate);
+		futureDate.setMonth(currentDate.getMonth() + months);
+		const futureMonthYear = futureDate.toLocaleString('en-US', {
+			month: 'long',
+			year: 'numeric',
+		});
+
+		console.log({
+			currentMonthYear,
+			futureMonthYear,
+			months,
+			offsetPerMonth,
+			monthlyEmissions,
+			monthlyCost,
+		});
+
+		// update the summary table
+		summaryTable.push({
+			month: futureMonthYear,
+			months: months,
+			trees: treeCount,
+			offset: offsetPerMonth.toFixed(2),
+		});
+		// store summary
+		if (offsetPerMonth >= monthlyEmissions) {
+			console.log(summaryTable);
+			localStorage.setItem('summaryTable', JSON.stringify(summaryTable));
+			const summary = [`Your average monthly`];
+		}
 	}
 };
 
