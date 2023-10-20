a970f6135e14dc53ca3d680cfddaad512c15e22a
Mohammad Zafar <mzcoder7@gmail.com>
2023-10-20 12:55:50 +0100

    Summary component started

--- simulator/src/components/Summary.jsx
+++ simulator/src/components/Summary.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Summary = () => {
-	return (
+	const [summary, setSummary] = useState(
+		() => localStorage.getItem('summary') || ''
+	);
+	return !summary ? (
 		<div className='grid grid-cols-[repeat(2,_1fr)] grid-rows-[repeat(4,_1fr)] border border-black w-[90vw] h-[60vw] md:w-[45vw] md:h-[30vw]'>
-			Summary
+			<p>Please submit data to generate a summary.</p>
+		</div>
+	) : (
+		<div className='grid grid-cols-[repeat(2,_1fr)] grid-rows-[repeat(4,_1fr)] border border-black w-[90vw] h-[60vw] md:w-[45vw] md:h-[30vw]'>
+			<p>Summary will appear here</p>
 		</div>
 	);
 };
---
+++ simulator/src/utils/functions.js
@@ -0,0 +1,12 @@
+const sampleInputs = {
+	country: 'United States',
+	annualConsumption: 15.52,
+	upfrontCost: 120,
+	annualCost: 12,
+	carbonOffset: 28.5,
+	treesPerMonth: 1,
+	timeToGrow: 5,
+	inflation: 3.7,
+};
+
+export const getSummary = inputs => {};
